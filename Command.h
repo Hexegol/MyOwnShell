023b2484917fe659d01574cf4ff6e7dad868c5e3
GlosXomegalomane <amrounaden@gmail.com>
2024-08-17 19:25:39 +0200

    Update Command.h

--- Command.h
+++ Command.h
@@ -23,3 +23,43 @@ public:
     CdCommand(const std::string& path);
     void execute(Shell& shell) override;
 };
+
+class LsCommand : public Command
+{
+public:
+    void execute(Shell& shell) override;
+};
+
+class PwdCommand : public Command
+{
+public:
+    void execute(Shell& shell) override;
+};
+
+class MkdirCommand : public Command
+{
+private:
+    std::string directoryName;
+public:
+    explicit MkdirCommand(const std::string& directoryName);
+    void execute(Shell& shell) override;
+};
+
+
+class RmdirCommand : public Command
+{
+private:
+    std::string directoryName;
+public:
+    explicit RmdirCommand(const std::string& directoryName);
+    void execute(Shell& shell) override;
+};
+
+class TouchCommand : public Command
+{
+private:
+    std::string fileName;
+public:
+    explicit TouchCommand(const std::string& fileName);
+    void execute(Shell& shell) override;
+};

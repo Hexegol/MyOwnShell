92dd2dced1374edf0d580b260439c6dfb61c3568
GlosXomegalomane <amrounaden@gmail.com>
2024-08-17 19:25:28 +0200

    Update Command.cpp

--- Command.cpp
+++ Command.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <filesystem>
 #include "Shell.h"
+#include <fstream>
 
 void ExitCommand::execute(Shell& shell)
 {
@@ -26,4 +27,91 @@ void CdCommand::execute(Shell& shell) {
     {
         std::cerr << "Error changing directory: " << e.what() << std::endl;
     }
+}
+
+void LsCommand::execute(Shell& shell)
+{
+    std::string path = ".";
+    for (const auto& entry : fs::directory_iterator(path)) {
+        std::cout << entry.path() << std::endl;
+    }
+}
+
+void PwdCommand::execute(Shell& shell)
+{
+    std::cout << std::filesystem::current_path() << std::endl;
+}
+
+MkdirCommand::MkdirCommand(const std::string& directoryName) : directoryName(directoryName) 
+{
+
+}
+void MkdirCommand::execute(Shell& shell)
+{
+    try
+    {
+        if (std::filesystem::create_directory(directoryName))
+        {
+            std::cout << "directory created : " << directoryName << std::endl;
+        }
+        else
+        {
+            std::cerr << "failed to create directory" << directoryName << std::endl;
+        }
+    }
+    catch (std::filesystem::filesystem_error& e)
+    {
+        std::cerr << "error creating directory : " << e.what() << std::endl;
+    }
+
+}
+
+RmdirCommand::RmdirCommand(const std::string& directoryName) : directoryName(directoryName)
+{
+
+}
+
+void RmdirCommand::execute(Shell& shell)
+{
+    if (std::filesystem::is_empty(directoryName))
+    {
+        try
+        {
+            if (std::filesystem::remove(directoryName))
+            {
+                std::cout << "empty directory removed " << std::endl;
+            }
+            else
+            {
+                std::cerr << "failed to remove directory" << directoryName << std::endl;
+            }
+        }
+        catch (std::filesystem::filesystem_error& e)
+        {
+            std::cerr << "error removing directory : " << e.what() << std::endl;
+        }
+    }
+    else
+    {
+        std::cerr << "directory is not empty" << std::endl;
+    }
+}
+
+
+TouchCommand::TouchCommand(const std::string& directoryName) : fileName(fileName)
+{
+
+}
+
+void TouchCommand::execute(Shell& shell)
+{
+    
+    std::ofstream file(fileName);
+    if (file) {
+        std::cout << "file touched : " << fileName << std::endl;
+    }
+    else {
+        std::cerr << "error : could not create or open file : " << fileName << std::endl;
+    }
+   
 }
